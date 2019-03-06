53f69f3bf4bc3a049ba1b38f43e96537f3063e10
Francis Chen <fncis.chen@gmail.com>
2019-03-06 14:02:11 -0800

    adding depenedicies onto index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 // needed dependancies
+
+import { applyMiddleware, createStore } from 'redux';
 // applyMiddleware from redux
+
+import thunk from 'redux-thunk';
 // thunk from redux-thunk
+
+import logger from 'redux-logger';
 // logger from redux-logger
+
 // rootReducer from ./reducers
 
 const store = createStore(
